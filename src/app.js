eb347cbdaadae986b53d45e682bd5921325945d0
Live session user <Zachary Gacer>
2017-05-16 04:14:41 +0000

    adding styling

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import {render} from 'react-dom'
+import './app.scss'
 
 class Hello extends React.Component {
   render () {
     return (
       <div>
-        <h1>Hello from React</h1>
+        <h1>Hello from React!</h1>
       </div>
     )
   }
